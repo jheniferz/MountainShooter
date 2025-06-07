9360941a8de299bd222f349487bd1c51efcc0d23
Jhenifer <jheniferlorrainy2005@gmail.com>
2025-06-07 18:50:07 -0300

    Adding classes and creating main loop and connecting menu and game classes

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import pygame
+from code.Game import Game
 
-print('Setup Star')
-pygame.init()
-window = pygame.display.set_mode(size=(600, 480))
-print('Setup End')
-
-print('Setup loop')
-while True:
-    # check fo all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()  # Close window
-            quit()  # end pygame
+game = Game()
+game.run()
