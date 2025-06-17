6c8db1ffa79e7a2600f5ec1d882be0627a225cee
Jhenifer <jheniferlorrainy2005@gmail.com>
2025-06-16 22:30:06 -0300

    Creating fyle Background Creating fyle Entity Creating fyle EntityFactory Creating Fyle Level Creating Menu Interations ABC Class Background parallax

--- code/Game.py
+++ code/Game.py
@@ -1,18 +1,27 @@
 import pygame
 from code.Menu import Menu
-from code.Const import WIN_WIDHT, WIN_HEIGHT
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.Level import Level
 
 
 class Game:
 
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(WIN_WIDHT, WIN_HEIGHT))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
-
+    def run(self):
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[4]:
+                pygame.quit()
+                quit()
+            else:
+                pass
 
 
