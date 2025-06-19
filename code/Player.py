e6dff4e4ba44c2188b8a9ebfa13fc153035ae078
Jhenifer <jheniferlorrainy2005@gmail.com>
2025-06-19 20:09:16 -0300

    Creating file/class "Player"

--- code/Player.py
+++ code/Player.py
@@ -1,15 +1,12 @@
-
 from Entity import Entity
 
 
 class Player(Entity):
-
     def __init__(self):
         pass
 
-    def updtate(self, ):
+    def update(self, ):
         pass
 
     def move(self, ):
         pass
-
